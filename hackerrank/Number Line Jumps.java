4b7ebfd433ddd3fcd9db507c45999dcf4ac74590
SRIKANTH C <62004664+srikanth6legend@users.noreply.github.com>
2026-02-06 10:59:31 +0530

    Update Number Line Jumps.java

--- hackerrank/Number Line Jumps.java
+++ hackerrank/Number Line Jumps.java
@@ -11,18 +11,6 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
-
-    /*
-     * Complete the 'kangaroo' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. INTEGER x1
-     *  2. INTEGER v1
-     *  3. INTEGER x2
-     *  4. INTEGER v2
-     */
-
     public static String kangaroo(int x1, int v1, int x2, int v2) {
 
         // If kangaroo 1 is behind and not faster, it can never catch up
